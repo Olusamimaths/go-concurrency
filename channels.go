73271e6bd44f790295372c2684b4272e688f1e74
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-01-21 11:00:58 +0100

    🔥 Select statement

--- channels.go
+++ channels.go
@@ -4,12 +4,23 @@ import "fmt"
 
 func main() {
 	myChannel := make(chan string)
+	anotherChannel := make(chan string)
 
 	go func() {
 		myChannel <- "data"
 	}()
 
-	msg := <-myChannel
+	go func ()  {
+		anotherChannel <- "dog"
+	}()
+
+	var msg string
+	select {
+	case msg := <- myChannel:
+		fmt.Println("Message from my channel, " + msg)
+	case msg := <- anotherChannel:
+		fmt.Println("Message from another channel, " + msg)
+	}
 
 	fmt.Println(msg)
 }
